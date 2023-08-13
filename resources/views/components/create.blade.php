686657ab7c2c2b136d85bdadc13f0d3c69b54fbd
Ertan Muslic <ertanmuslic@gmail.com>
2023-08-14 00:36:12 +0200

    Keep old data in Form

--- resources/views/components/create.blade.php
+++ resources/views/components/create.blade.php
@@ -4,35 +4,35 @@ To be Created....
 <form method="POST" action="/courses">
     @csrf
     <label for="title">Naziv kursa:</label>
-    <input type="text" id="title" name="title"><br><br>
+    <input type="text" id="title" name="title" value="{{old('title')}}"><br><br>
 
     @error('title')
     <p class="errorMessage">{{$message}}</p>
     @enderror
 
     <label for="description">Opis kursa:</label><br>
-    <textarea id="description" name="description" rows="4" cols="50"></textarea><br><br>
+    <textarea id="description" name="description" rows="4" cols="50" value="{{old('description')}}"></textarea><br><br>
 
     @error('description')
     <p class="errorMessage">{{$message}}</p>
     @enderror
 
     <label for="duration">Trajanje kursa:</label>
-    <input type="text" id="duration" name="duration"><br><br>
+    <input type="text" id="duration" name="duration" value="{{old('duration')}}"><br><br>
 
     @error('duration')
     <p class="errorMessage">{{$message}}</p>
     @enderror
 
     <label for="tags">Tagovi (odvojeni zarezima):</label>
-    <input type="text" id="tags" name="tags"><br><br>
+    <input type="text" id="tags" name="tags" value="{{old('tags')}}"><br><br>
 
     @error('tags')
     <p class="errorMessage">{{$message}}</p>
     @enderror
 
     <label for="price">Cena kursa:</label>
-    <input type="number" id="price" name="price" min="0" step="0.01"><br><br>
+    <input type="number" id="price" name="price" min="0" step="0.01" value="{{old('price')}}"><br><br>
 
     @error('price')
     <p class="errorMessage">{{$message}}</p>
