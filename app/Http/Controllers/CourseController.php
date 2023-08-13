247f7dbb136efda3a50be1bf6089dde5c3987b06
Ertan Muslic <ertanmuslic@gmail.com>
2023-08-13 23:02:05 +0200

    search filter

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -8,24 +8,33 @@ use Illuminate\Validation\Rule;
 
 class CourseController extends Controller
 {
-    //
+
+    public function home(){
+        return view('home');
+    }
+
+
+    //Get All Courses
     public function index(){
         return view('components/courses/index', [
-            'courses' => Courses::latest()->filter(request(['tag']))
+            'courses' => Courses::latest()->filter(request(['tag', 'search']))
             ->get()
         ]);
     }
-
+    
+    //Show Form For Course Creation
     public function create(){
         return view('components/create');
     }
 
+    //Get Single Course
     public function show(Courses $course){
         return view('components/courses/show' , [
             'course' => $course
         ]);
     }
 
+    //Create Course
     public function store(Request $request){
         // dd($request->all());
         $formFields = $request-> validate([
