e309c42d1dc001bcd5fb9ab583dcb95648d15117
Ertan Muslic <ertanmuslic@gmail.com>
2023-08-16 22:49:46 +0200

    navbar css

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -4,12 +4,25 @@
 <link href="https://fonts.googleapis.com/css2?family=Elsie&family=Space+Grotesk:wght@300&display=swap" rel="stylesheet">
 <nav class="navbar">
     <div class="container">
-        <a href="#" class="logo">Online Courses</a>
-        <ul class="nav-links">
+        <a href="/" class="logo">Online Courses</a>
+        <ul class="nav-links">                
             <li><a href="/">Home</a></li>
             <li><a href="/courses">Courses</a></li>
-            <li><a href="/">About</a></li>
-            <li><a href="/">Contact</a></li>
+            @auth
+            <li><span class="user"> <i class="fa-solid fa-user"></i> {{auth()->user()->name}}</span></li>
+            <li><a href="/courses/manage"><i class="fa-solid fa-gear"></i> Manage Courses</a></li>
+            <li>
+                <form action="/logout">
+                    @csrf
+                    <button type="submit">
+                        <i class="fa-solid fa-door-closed"></i> Logout
+                    </button>
+                </form>
+            </li>
+            @else
+            <li><a href="/register"><i class="fa-solid fa-user-plus"></i> Register</a></li>
+            <li><a href="/login"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a></li>
+            @endauth
         </ul>
     </div>
 </nav>
