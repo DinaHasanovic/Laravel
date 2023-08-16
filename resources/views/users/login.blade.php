6e7854f73298eddcd30543d56d90164d264d79bd
Ertan Muslic <ertanmuslic@gmail.com>
2023-08-16 22:49:23 +0200

    login form css finish

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,20 +1,31 @@
 <x-layout>
-    <h2>User Registration</h2>
-    <form  method="post" action="/users/authenticate">
-        @csrf
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" value="{{old('email')}}">
-        @error('email')
-        <p style="color: red">{{$message}}</p>
-        @enderror
-        <br>
+    <link rel="stylesheet" href="{{ asset('css/loginForm.css') }}">
+    <div class="form_body">
+        <div class="form_container">
+            <div>
+            <h2>User Registration</h2>
+            </div>
+            <form  method="post" action="/users/authenticate">
+            @csrf
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" value="{{old('email')}}">
+            @error('email')
+            <p style="color: red">{{$message}}</p>
+            @enderror
+            <br>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" value="{{old('password')}}">
-        @error('password')
-        <p style="color: red">{{$message}}</p>
-        @enderror
-        <br>
-        <button type="submit">Login</button>
-    </form>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" value="{{old('password')}}">
+            @error('password')
+            <p style="color: red">{{$message}}</p>
+            @enderror
+            <br>
+
+            <a>Forget Password?</a>
+            <button type="submit">Login</button>
+
+            <p style="text-align: center;">Not a Member?  <a style="padding-left: 2%" href="/register">Sign In</a></p>
+            </form>
+        </div>
+</div>
 </x-layout>
